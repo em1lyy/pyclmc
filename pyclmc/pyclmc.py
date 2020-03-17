6f736673052508e1612934af7447f7cb0fbeb11d
jonasjaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-03-17 16:24:33 +0100

    Added a lot
    new things:
    - working audio via mplayer
    - mplayer control functions
    - updating header!
    
    what the program does rn:
    - init curses "ui"
    - mplay 60 seconds of Listen.MOE
    - quit
    
    what's gonna be in the next version(s):
    - keyboard shortcuts
    - better (and more) ui
    - websocket metadata fetching
    - (maybe) song cover display

--- pyclmc/pyclmc.py
+++ pyclmc/pyclmc.py
@@ -6,17 +6,24 @@
 # Released under GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
 # Maintained by Jonas Jaguar <jonasjaguar@jagudev.net>, 2020
 
-import curses
 from time import sleep
+import curses
+from subprocess import Popen, PIPE, DEVNULL
+import errno
 
-HEADER_TEXT = "Not playing - pyclmc"
+HEADER_TEXT = ""
 
 def main():
     stdscr = _init_curses()
-    stdscr.addstr(0, 0, int(curses.COLS/2-(len(HEADER_TEXT)/2))*" " + HEADER_TEXT + int(curses.COLS/2-(len(HEADER_TEXT)/2)) * " ",
-              curses.A_REVERSE)
-    stdscr.refresh()
-    sleep(5)
+
+    set_header_text(stdscr, "Initializing... - pyclmc")
+
+    mplayer_process = _init_mplayer_with_pipe()
+    set_header_text(stdscr, "Playing - pyclmc")
+    sleep(60)
+
+    set_header_text(stdscr, "Quitting... - pyclmc")
+    _quit_mplayer(mplayer_process)
     _quit_curses(stdscr)
 
 def _init_curses():
@@ -32,6 +39,70 @@ def _quit_curses(stdscr):  # Revert to terminal-friendly mode
     stdscr.keypad(False)
     curses.echo()
     curses.endwin()
+
+def set_header_text(stdscr, text):
+    HEADER_TEXT = text
+    stdscr.addstr(0, 0, int(curses.COLS/2-(len(HEADER_TEXT)/2))*" " + HEADER_TEXT + int(curses.COLS/2-(len(HEADER_TEXT)/2)) * " ",
+              curses.A_REVERSE)
+    stdscr.refresh()
+
+def _init_mplayer_with_pipe():
+    p = Popen('mplayer https://listen.moe/stream', stdin=PIPE)
+    return p
+
+def _quit_mplayer(proc):
+    line = b'q'
+    try:
+        proc.stdin.write(line)
+    except IOError as e:
+        proc.kill()
+    proc.stdin.flush()
+    proc.stdin.close()
+    proc.wait()
+
+def mplayer_incvol(proc):
+    line = b'0'
+    try:
+        proc.stdin.write(line)
+    except IOError as e:
+        if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+            return
+        else:
+            raise
+    proc.stdin.flush()
+
+def mplayer_decvol(proc):
+    line = b'9'
+    try:
+        proc.stdin.write(line)
+    except IOError as e:
+        if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+            return
+        else:
+            raise
+    proc.stdin.flush()
+
+def mplayer_playpause(proc):
+    line = b' '
+    try:
+        proc.stdin.write(line)
+    except IOError as e:
+        if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+            return
+        else:
+            raise
+    proc.stdin.flush()
+
+def mplayer_mutetoggle(proc):
+    line = b'm'
+    try:
+        proc.stdin.write(line)
+    except IOError as e:
+        if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+            return
+        else:
+            raise
+    proc.stdin.flush()
 
 if __name__ == '__main__':
     main()
